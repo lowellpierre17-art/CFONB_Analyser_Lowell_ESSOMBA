7189ee61b8893b7e9f76602cbadfc30918adc46e
NICKSON <u4717711212@gmail.com>
2026-01-02 04:00:41 +0100

    Structure et os exigneces entre nouvequ solde; operqtion et qncient solfes

--- main.c
+++ main.c
@@ -1,51 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+
 #include "cfonb_parser.h"
 #include "cfonb_stats.h"
 #include "cfonb_types.h"
 #include "cfonb_utils.h"
 #include "cfonb_validator.h"
 
-int main() {
-
-    FichierCFONB* fichier = chargerFichier("D://data//file (30).txt");
-    if (fichier == NULL)
-        printf("Error parsing file!\n");
-    else {
-        afficherChargement(fichier);
-    }
-    libererFichier(fichier);
-    return 0;
-}
-
-
-/*Arguments args;
+int main(int argc, char *argv[]) {
+    Arguments args;
     int resultat = parserArguments(argc, argv, &args);
     if (resultat <= 0) {
         afficherUsage(argv[0]);
         return (resultat < 0) ? 1 : 0;
     }
+    afficherArguments(&args);
+    printf("\n");
     // Exécuter la commande demandée
     if (strcmp(args.commande, "load") == 0) {
-        // chargerEtAfficher(args.fichier);
-        printf("Chargement de %s...\n", args.fichier);
+        chargerEtAfficher(args.fichier);
     }
     else if (strcmp(args.commande, "validate") == 0) {
-        // validerFichier(args.fichier);
-        printf("Validation de %s...\n", args.fichier);
+        afficherValiderFichier(args.fichier);
     }
     else if (strcmp(args.commande, "stats") == 0) {
-        // afficherStats(args.fichier);
+        afficherStats(args.fichier);
         printf("Statistiques de %s...\n", args.fichier);
     }
     else if (strcmp(args.commande, "search") == 0) {
-        // rechercherOperations(args.fichier, &args);
-        printf("Recherche dans %s...\n", args.fichier);
-        if (args.numeroCompte[0] != '\0') {
-            printf("  Filtre compte : %s\n", args.numeroCompte);
-        }
+        afficherRechercherOperations(args.fichier, &args);
     }
     else {
         printf("Erreur : commande '%s' inconnue\n", args.commande);
         afficherUsage(argv[0]);
         return 1;
-    }*/
+    }
+    return 0;
+}
